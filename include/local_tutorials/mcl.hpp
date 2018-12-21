b4898f6df61ffb279fb907c45f085afbc4a2cc91
kari <12kari20@gmail.com>
2018-12-22 02:11:17 +0900

    new

--- include/local_tutorials/mcl.hpp
+++ include/local_tutorials/mcl.hpp
@@ -2,8 +2,10 @@
 #define _MCL_HPP_
 #include<iostream>
 #include<ros/ros.h>
-// #include <dynamic_reconfigure/server.h>
-// #include <local_tutorials/ParamConfig.h>
+#include<geometry_msgs/PoseArray.h>
+#include<geometry_msgs/Pose.h>
+#include<dynamic_reconfigure/server.h>
+#include<local_tutorials/mcl_paramConfig.h>
 
 using namespace std;
 
@@ -18,10 +20,19 @@ struct Particle{
 class MCL{
 
 	private:
+		dynamic_reconfigure::Server<local_tutorials::mcl_paramConfig> server;
+		ros::Publisher pf_pub;
+		geometry_msgs::PoseArray pf_array;
+
+		int pf_num;
+
 		Particle pf_state();
+		geometry_msgs::Pose pf2geo(Particle pf);
+
 	public:
 		MCL(ros::NodeHandle n,ros::NodeHandle priv_nh);
 		void pub();
+		void paramcallback(local_tutorials::mcl_paramConfig &config, uint32_t level);
 };
 
 #endif
--- src/mcl.cpp
+++ src/mcl.cpp
@@ -2,7 +2,20 @@
 
 MCL::MCL(ros::NodeHandle n,ros::NodeHandle priv_nh)
 {
+	server.setCallback(boost::bind(&MCL::paramcallback, this, _1, _2));
+	pf_pub = n.advertise<geometry_msgs::PoseArray>("/pf_cloud",100);
+
+	pf_array.header.frame_id = "/map";
 }
+
+void 
+MCL::paramcallback(local_tutorials::mcl_paramConfig &config, uint32_t level) {
+	ROS_INFO("Reconfigure Request: %d %d", 
+			config.particle_num, config.const_num);
+
+	pf_num = config.particle_num;
+}
+
 
 Particle
 MCL::pf_state(){
@@ -10,7 +23,7 @@ MCL::pf_state(){
 
 	std::random_device rnd;
 	mt19937 mt(rnd());
-	uniform_real_distribution<double> score(0.0,1.0);
+	uniform_real_distribution<double> score(-5.0,5.0);
 	
 	pf.x = score(mt);
 	pf.y = score(mt);
@@ -20,11 +33,34 @@ MCL::pf_state(){
 	return pf;
 }
 
+geometry_msgs::Pose
+MCL::pf2geo(Particle pf){
+	geometry_msgs::Pose pf_geo;
+	
+	pf_geo.position.x = pf.x;
+	pf_geo.position.y = pf.y;
+	pf_geo.position.z = 0.0;
+	pf_geo.orientation.x = 0.0;
+	pf_geo.orientation.y = 0.0;
+	pf_geo.orientation.z = 0.0;
+	pf_geo.orientation.w = 1.0;
+
+	return pf_geo;
+}
+
 void
 MCL::pub(){
 	Particle pf;
-	pf = pf_state();
+	geometry_msgs::Pose pf_geo;
 
-	cout<<pf.x<<","<<pf.y<<endl;
+	pf_array.poses.clear();
+	for(int i=0;i<pf_num;i++){
+		pf = pf_state();
+		pf_geo = pf2geo(pf);
+
+		pf_array.poses.push_back(pf_geo);
+	}
+	pf_array.header.stamp = ros::Time::now();
+	pf_pub.publish(pf_array);
 	
 }
