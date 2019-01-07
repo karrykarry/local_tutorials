57e58ff7a6ea4f5d0d60dcdcbcd7df0517aa53b1
kari <12kari20@gmail.com>
2019-01-07 22:06:00 +0900

    new

---
+++ src/vicon_result.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+
+using namespace std;
+
+class Vicon{
+	private:
+		ros::Subscriber sub_;
+		tf::TransformBroadcaster tfbroadcaster_;
+	public:
+		Vicon(ros::NodeHandle n,ros::NodeHandle private_nh);
+		void callback(const geometry_msgs::TransformStamped::Ptr msg);
+};
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "vicon_odom");
+	ros::NodeHandle n;
+	ros::NodeHandle private_nh("~");
+
+	Vicon vo(n,private_nh);
+
+	ros::spin();
+
+	return 0;  
+}
+
+Vicon::Vicon(ros::NodeHandle n,ros::NodeHandle private_nh_)
+{
+	sub_ = n.subscribe("/vicon_topic", 2, &Vicon::callback, this);
+
+	cout<<"vicon start"<<endl;
+}
+
+void
+Vicon::callback(const geometry_msgs::TransformStamped::Ptr msg){
+
+	geometry_msgs::Quaternion q;
+	q = msg->transform.rotation;
+
+    double roll,pitch,yaw;
+	tf::Quaternion quat(q.x,q.y,q.z,q.w);
+	tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+
+	cout <<"yaw:"<<yaw<<endl;
+	
+	geometry_msgs::TransformStamped st;
+	
+	st = *msg;
+	st.header.frame_id = "/velodyne";
+	st.child_frame_id = "/vicon";
+
+	tfbroadcaster_.sendTransform(st);
+}
+
+
